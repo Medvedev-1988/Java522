16c4a15b6e48d197abd94b8c7632b95f17c6a520
Dmitriy <Dima_Medvedev.1988@mail.ru>
2025-07-06 18:37:59 +0300

    first commit

---
+++ 1- Intro/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Дима");
+        System.out.println("36 лет");
+        System.out.println("Балашиха");
+        System.out.println("улекаюсь боксом");
+        System.out.println("Люблю поесть");
+    }
+}
