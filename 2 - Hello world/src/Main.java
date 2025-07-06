6d22d961a0d01905eec1a07c0896a08dcf5788b3
Dmitriy <Dima_Medvedev.1988@mail.ru>
2025-07-06 19:01:19 +0300

    update

---
+++ 2 - Hello world/src/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+    public static void main(String[] args) {
+//     1)
+        System.out.println("""
+                    *
+                   ***
+                  *****
+                 *******
+                ********* 
+                """);
+//      2)
+        System.out.println("""
+                *
+                **
+                ***
+                ****
+                *****
+                """);
+//      3)
+        System.out.println("""
+                ****
+                ***
+                **
+                *
+                """);
+//      4)
+        System.out.println("""
+                *
+                  *
+                    *
+                      *
+                        *
+                """);
+//      5)
+        System.out.println("""
+                    *
+                   *
+                  *
+                 *
+                *    
+                """);
+//      6)
+        System.out.println("""
+                           **
+                     *            *    
+                  *                  * 
+                *                      *
+                *                      *
+                  *                  * 
+                     *            * 
+                           **      
+                """);
+//      7)
+        System.out.println("""
+                  *
+                 ***
+                *****
+                 ***
+                  * 
+                """);
+//      8)
+        System.out.println("""
+                ******
+                ******
+                ******
+                """);
+    }
+}
