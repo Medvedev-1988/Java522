4f948ea432cb2ae0dbef6fca9f703872a04a5f15
dima-1988 <diman-88@mail.ru>
2025-08-12 22:53:07 +0300

    update

---
+++ 25 - Classes_untitled/src/Main.java
@@ -0,0 +1,184 @@
+//Classes
+//Outer class
+//Inner class
+// 1 Inner class
+// 2 Static Nested class
+//Local class
+//Anonym class
+
+//Outer class  vnesniy class
+//class OuterClass {
+//    public int a = 1;
+//    private int b = 1;
+//    protected int c = 1;
+//    int d = 1;
+//    static int e = 1;
+//
+//    //Inner class  vlojenniy class
+//    class InnerClass {
+//        {
+//            a = 1;
+//            b = 1;
+//            c = 1;
+//            e = 1;
+//            d = 1;
+//
+//            createInstance();
+//        }
+//    }
+//
+//    public void test() {
+//        class LocalClass {
+//            {
+//                a = 1;
+//                b = 1;
+//                c = 1;
+//                e = 1;
+//                d = 1;
+//
+//                createInstance();
+//            }
+//        }
+//    }
+//
+//    public InnerClass createInstance() {
+//        return new InnerClass();
+//    }
+//
+//    //Static Nested class staticekiy vlojenniy class
+//    static class StaticInnerClass {
+//        {
+//            e = 1;
+//        }
+//    }
+//}
+//
+//
+//abstract class Test {
+//    abstract void test();
+//}
+//
+//
+//interface Printable{
+//    void  print();
+//}
+//
+////enum Color{
+////
+////}
+//
+//
+//final class Print{
+//
+//}
+//
+//
+//abstract class T{
+//
+//}
+//
+
+//class Person{
+//    final String name;
+//    final String lastname;
+//
+//    public Person(String name, String lastname) {
+//        this.name = name;
+//        this.lastname = lastname;
+//    }
+//
+//    public String getLastname() {
+//        return lastname;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//}
+
+//record Person(String name,String surname){}
+
+
+        public class Main {
+            public static void main(String[] args) {
+                //Outer class
+                //Inner class
+                // 1. Static Nested class
+                // 2. Inner class
+                //Local class
+                //Anonym class
+                //Abstract class
+                //Final class
+                //Enum
+                //Record class
+
+
+                //DTO (Data Transfer Object)
+//        Person person = new Person("Farid","Abdullayev");
+
+
+//        Printable printable = new Printable() {
+//            @Override
+//            public void print() {
+//                System.out.println("Test");
+//            }
+//        };
+
+
+//        printable.print();;
+//        Printable printable = new Printable();
+//        Test test = new Test();
+//        Test test = new Test() {
+//            @Override
+//            void test() {
+//                System.out.println("OK");
+//            }
+//        };
+
+
+//        test.test();
+
+                //        OuterClass outerClass = new OuterClass();
+//        OuterClass.InnerClass innerClass = outerClass.createInstance();
+
+
+//        if (true){
+//            class Test{
+//
+//            }
+//
+//
+//            Test test = new Test();
+////            test.
+//        }
+
+//        Test test = new Test
+//
+//        OuterClass.StaticInnerClass staticInnerClass = new OuterClass.StaticInnerClass();
+//        Person.Brain brain =  new Person.Brain();
+            }
+        }
+//class Person{
+//    class Heart{
+//
+//    }
+//
+//
+//    class Brain{
+//
+//    }
+//}
+
+
+//static class test{
+//
+//}
+//
+//private class Tes{
+//
+//}
+
+//
+//public class Test{
+//
+//}
